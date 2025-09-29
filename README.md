9a200eb4244e8b8ff1c7f4afaa7400679679ca49
Bashkim Isai <me@bashkim.com>
2025-09-29 16:35:35 +0200

    chore: migrate to typescript

--- README.md
+++ README.md
@@ -12,17 +12,16 @@ npm install posthtml posthtml-external-link-target-blank
 
 ### Example
 
-```javascript
-const posthtml = require('posthtml');
-const externalLinks = require('posthtml-external-link-target-blank');
+```typescript
+import posthtml from "posthtml";
+import externalLinks from "posthtml-external-link-target-blank";
 
-const inputHTML = ;
-const outputHTML = posthtml()
-  .use(externalLinks())
-  .process('<a href="http://www.example.com/">lorem ipsum dolar sit a met</div>', { sync: true })
-  .html;
+const inputHTML =
+  '<a href="http://www.example.com/">lorem ipsum dolar sit a met</div>';
 
-// <a href="http://www.example.com/" target="_blank" rel="noopener">lorem ipsum dolar sit a met</div>
+const output = await posthtml().use(externalLinks()).process(inputHTML);
+const outputHTML = output.html;
+// <a href="http://www.example.com/" target="_blank" rel="noopener noreferrer">lorem ipsum dolar sit a met</div>
 ```
 
 ### Options
@@ -31,26 +30,40 @@ const outputHTML = posthtml()
 
 An array of hosts which should be excluded from opening new windows.
 
-* Since: `1.0.0`
-* Property is `Optional`
-* Default value is: `[]`
+- Since: `1.0.0`
+- Property is `Optional`
+- Default value is: `[]`
 
 Example:
 
-```javascript
-const posthtml = require('posthtml');
-const externalLinks = require('posthtml-external-link-target-blank');
+```typescript
+import posthtml from "posthtml";
+import externalLinks from "posthtml-external-link-target-blank";
 
-const inputHTML = '<a href="http://www.example.com/">lorem ipsum dolar sit a met</div>';
-const outputHTML = posthtml()
-  .use(externalLinks({ excludeHosts: [ 'example.com', 'www.example.com' ] }))
-  .process(inputHTML, { sync: true })
-  .html;
+const inputHTML =
+  '<a href="http://www.example.com/">lorem ipsum dolar sit a met</div>';
 
-
+const output = await posthtml()
+  .use(externalLinks({ excludeHosts: ["example.com", "www.example.com"] }))
+  .process(inputHTML);
 ```
 
-[url:posthtml]: https://github.com/posthtml/posthtml
+#### noopener
 
+Whether or not to include `rel="noopener"` in the link tag.
+
+- Since: `1.1.0`
+- Property is `Optional`
+- Default value is: `true`
+
+#### noreferrer
+
+Whether or not to include `rel="noreferrer"` in the link tag.
+
+- Since: `1.1.0`
+- Property is `Optional`
+- Default value is: `true`
+
+[url:posthtml]: https://github.com/posthtml/posthtml
 [img:npm]: https://img.shields.io/npm/v/posthtml-external-link-target-blank.svg
 [url:npm]: https://www.npmjs.com/package/posthtml-external-link-target-blank
---
+++ src/index.spec.ts
@@ -0,0 +1,153 @@
+import posthtml from "posthtml";
+import ExternalLinks from ".";
+
+describe("new window external link", () => {
+  it("generates with default values", async () => {
+    const output = await transform(
+      '<a href="https://www.example.com">Example</a>',
+      {},
+    );
+
+    expect(output).toEqual(
+      '<a href="https://www.example.com" target="_blank" rel="noopener noreferrer">Example</a>',
+    );
+  });
+
+  it("ignores tags without href attributes", async () => {
+    const output = await transform("<a>Example</a>");
+    expect(output).toEqual("<a>Example</a>");
+  });
+
+  it("keeps existing rel tags", async () => {
+    const output = await transform('<a rel="noopener">Example</a>');
+    expect(output).toEqual('<a rel="noopener">Example</a>');
+  });
+
+  it("marks up external links", async () => {
+    const output = await transform(
+      `<a href="http://www.example.com/">Example</a>`,
+    );
+
+    expect(output).toEqual(
+      `<a href="http://www.example.com/" target="_blank" rel="noopener noreferrer">Example</a>`,
+    );
+  });
+
+  describe("internal links", () => {
+    it("skips root path", async () => {
+      const output = await transform('<a href="/">innerText</a>');
+      expect(output).toEqual('<a href="/">innerText</a>');
+    });
+
+    it("skips paths", async () => {
+      const output = await transform('<a href="/path/">innerText</a>');
+      expect(output).toEqual('<a href="/path/">innerText</a>');
+    });
+
+    it("skips files", async () => {
+      const output = await transform('<a href="path.html">innerText</a>');
+      expect(output).toEqual('<a href="path.html">innerText</a>');
+    });
+  });
+
+  describe("protocols", () => {
+    it("honors the protocol", async () => {
+      const output = await transform('<a href="//www.example.com/"></a>');
+      expect(output).toEqual(
+        '<a href="//www.example.com/" target="_blank" rel="noopener noreferrer"></a>',
+      );
+    });
+
+    it("keeps the https protocol", async () => {
+      const output = await transform('<a href="https://www.example.com/"></a>');
+      expect(output).toEqual(
+        '<a href="https://www.example.com/" target="_blank" rel="noopener noreferrer"></a>',
+      );
+    });
+  });
+
+  describe("#excludeHosts", () => {
+    it("ignores a single domain", async () => {
+      const output = await transform('<a href="http://www.example.com/"></a>', {
+        excludeHosts: ["localhost"],
+      });
+
+      expect(output).toEqual(
+        '<a href="http://www.example.com/" target="_blank" rel="noopener noreferrer"></a>',
+      );
+    });
+
+    it("ignores multiple domains", async () => {
+      const output = await transform(
+        [
+          `<a href="http://localhost/"></a>`,
+          `<a href="http://broadcasthost/"></a>`,
+          `<a href="http://www.example.com/"></a>`,
+        ].join(" "),
+        { excludeHosts: ["localhost", "broadcasthost"] },
+      );
+
+      expect(output).toEqual(
+        [
+          `<a href="http://localhost/"></a>`,
+          `<a href="http://broadcasthost/"></a>`,
+          `<a href="http://www.example.com/" target="_blank" rel="noopener noreferrer"></a>`,
+        ].join(" "),
+      );
+    });
+  });
+
+  describe("#noopener", () => {
+    it("includes when true", async () => {
+      const output = await transform('<a href="http://www.example.com/"></a>', {
+        noopener: true,
+        noreferrer: true,
+      });
+
+      expect(output).toEqual(
+        '<a href="http://www.example.com/" target="_blank" rel="noopener noreferrer"></a>',
+      );
+    });
+
+    it("excludes when true", async () => {
+      const output = await transform('<a href="http://www.example.com/"></a>', {
+        noopener: false,
+        noreferrer: true,
+      });
+
+      expect(output).toEqual(
+        '<a href="http://www.example.com/" target="_blank" rel="noreferrer"></a>',
+      );
+    });
+  });
+
+  describe("#noreferrer", () => {
+    it("includes when true", async () => {
+      const output = await transform('<a href="http://www.example.com/"></a>', {
+        noopener: true,
+        noreferrer: true,
+      });
+
+      expect(output).toEqual(
+        '<a href="http://www.example.com/" target="_blank" rel="noopener noreferrer"></a>',
+      );
+    });
+
+    it("excludes when true", async () => {
+      const output = await transform('<a href="http://www.example.com/"></a>', {
+        noopener: true,
+        noreferrer: false,
+      });
+
+      expect(output).toEqual(
+        '<a href="http://www.example.com/" target="_blank" rel="noopener"></a>',
+      );
+    });
+  });
+});
+
+async function transform(input: string, options = {}) {
+  const response = await posthtml().use(ExternalLinks(options)).process(input);
+
+  return response.html;
+}
